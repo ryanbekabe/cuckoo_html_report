5e426330e8c382ee99aedbcfb61b087d0076f3a5
ryanbekabe <bekabeipa@gmail.com>
2019-10-23 16:15:05 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+<title>
+</title>
+</head>
+<body>
+<form action="upload.php"method="POST" enctype="multipart/form-data">
+<input type="file" name="file"><br><br>
+<button type="submit" name="submit">Upload</button>
+</form>
+</body>
+</html>
